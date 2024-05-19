50292ee85acb3239083781967394492297caff09
Xavier Fontanet <xavier.fontanet@edreamsodigeo.com>
2024-05-19 13:12:47 +0200

    first run

--- src/main/java/tv/mangrana/worker/MainWorker.java
+++ src/main/java/tv/mangrana/worker/MainWorker.java
@@ -1,3 +1,20 @@
 package tv.mangrana.worker;
+
+import tv.mangrana.config.ConfigFileLoader;
+import tv.mangrana.exception.IncorrectWorkingReferencesException;
+import tv.mangrana.sonarr.api.client.gateway.SonarrApiGateway;
+
 public class MainWorker {
+    private final SonarrApiGateway sonarrApiGateway;
+
+    public MainWorker() throws IncorrectWorkingReferencesException {
+        ConfigFileLoader configFileLoader = new ConfigFileLoader();
+        sonarrApiGateway = new SonarrApiGateway(configFileLoader);
+    }
+
+    public static void main(String[] args) throws IncorrectWorkingReferencesException {
+        var worker = new MainWorker();
+        var queue = worker.sonarrApiGateway.getQueue();
+        System.out.println(queue);
+    }
 }
